31a6f1967074d16d32a6c1d8dc9654de55f02e48
Jean Joe Irascible <manzede@gmail.com>
2018-08-31 18:43:16 +0300

    [Delivers #160200364] create empty lists and store file items to them

--- events.py
+++ events.py
@@ -1,2 +1,14 @@
 vip_file = open("files/vip_list.txt", "r")
 ordinary_file = open("files/ordinary_list.txt", "r")
+
+vip_names = []
+ordinary_names = []
+
+for line in vip_file:
+    vip_names.append(line)
+
+for line in ordinary_file:
+    ordinary_names.append(line)
+
+print(vip_names[2])
+print(ordinary_names[0])
