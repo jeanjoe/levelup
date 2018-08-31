04d9c869c1430a87deeedc47c4a14acc98486906
Jean Joe Irascible <manzede@gmail.com>
2018-08-31 19:41:15 +0300

    [Delivers #160200364] add registration_checker method and return full name

--- events.py
+++ events.py
@@ -1,17 +1,30 @@
 vip_file = open("files/vip_list.txt", "r")
 ordinary_file = open("files/ordinary_list.txt", "r")
 
-name = input("Enter your name: \t")
+input_name = input("Enter your name: \t")
 
 vip_names = []
 ordinary_names = []
 
 for line in vip_file:
     vip_names.append(line)
-    if line.find(name) != -1:
-        print(line)
 
 for line in ordinary_file:
     ordinary_names.append(line)
-    if line.find(name) != -1:
-        print(line)
+
+def registration_checker(name, data):
+    full_name = None
+    for fname in data:
+        if fname.find(name) != -1:
+            full_name = fname
+    
+    return full_name
+
+if registration_checker(input_name, vip_names) == None:
+    if registration_checker(input_name, ordinary_names) == None:
+        print("No Name match")
+    else:
+        print(registration_checker(input_name, ordinary_names))
+else:
+    print(registration_checker(input_name, vip_names))
+    
