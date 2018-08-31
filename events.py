f3973a0f9d5d33ce22633c665472e7a513a9ea9b
Jean Joe Irascible <manzede@gmail.com>
2018-08-31 19:10:41 +0300

    [Delivers #160200364] prompt user name nad print outpu

--- events.py
+++ events.py
@@ -1,14 +1,17 @@
 vip_file = open("files/vip_list.txt", "r")
 ordinary_file = open("files/ordinary_list.txt", "r")
 
+name = input("Enter your name: \t")
+
 vip_names = []
 ordinary_names = []
 
 for line in vip_file:
     vip_names.append(line)
+    if line.find(name) != -1:
+        print(line)
 
 for line in ordinary_file:
     ordinary_names.append(line)
-
-print(vip_names[2])
-print(ordinary_names[0])
+    if line.find(name) != -1:
+        print(line)
