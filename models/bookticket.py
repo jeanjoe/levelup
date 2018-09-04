ffb4f16fd1087061e9f04ae58293fe8210699da5
Jean Joe Irascible <manzede@gmail.com>
2018-09-04 17:45:39 +0300

    [Delivers #160244221] add book_ticket function and test

--- models/bookticket.py
+++ models/bookticket.py
@@ -4,3 +4,13 @@ class BookTicket():
     """Constructor for initializing global variables"""
     def __init__(self):
         self.tickets = []
+
+    """ Function to book ticket"""
+    def book_ticket(self, name, email):
+        visitor = {
+            "fullname": name,
+            "email": email
+        }
+        self.tickets.append(visitor)
+        return self.tickets
+        
--- tests/tests.py
+++ tests/tests.py
@@ -5,8 +5,16 @@ class TestBookTicket(unittest.TestCase):
 
     def test_book_ticket_class(self):
         bookticket = BookTicket()
-        self.assertIsInstance(bookticket, BookTicket())
+        self.assertIsInstance(bookticket, BookTicket)
 
     def test_instance_of_tickets_to_be_list(self):
         bookTicket = BookTicket()
         self.assertIsInstance(bookTicket.tickets, list)
+
+    def test_book_ticket(self):
+        bookTicket = BookTicket()
+        
+        self.assertEqual(bookTicket.book_ticket("Manzede Benard", "manzede@gmail.com"), 
+            [ {"fullname": "Manzede Benard", "email": "manzede@gmail.com" }])
+        self.assertEqual(len(bookTicket.tickets), 1)
+
