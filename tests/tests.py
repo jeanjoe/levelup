63eb214a3ae14e7b005bfc75c23811517806ac95
Jean Joe Irascible <manzede@gmail.com>
2018-09-06 11:20:35 +0300

    [Fixes #160293092] change comments

--- tests/tests.py
+++ tests/tests.py
@@ -3,53 +3,53 @@ from models.bookticket import BookTicket
 
 class TestBookTicket(unittest.TestCase):
 
-    #Instanciate BookTicket class and add a visitor
     def setUp(self):
+        """Instanciate BookTicket class and add a visitor."""
         self.bookTicket = BookTicket()
         self.bookTicket.add_ticket("Manzede Benard", "manzede@gmail.com")
 
-    #Test existence of BookTicket class
     def test_book_ticket_class(self):
+        """Test existence of BookTicket class."""
         self.assertIsInstance(self.bookTicket, BookTicket)
 
-    #Test tickets to be instance of list
     def test_instance_of_tickets_to_be_list(self):
+        """Test tickets to be instance of list."""
         self.assertIsInstance(self.bookTicket.tickets, list)
 
-    #Test length of tickets
     def test_length_of_tickets(self):
+        """Test length of tickets."""
         self.assertEqual(len(self.bookTicket.tickets), 1)
 
-    #Test add Ticket to the list
     def test_add_ticket(self):
+        """Test add Ticket to the list."""
         self.assertEqual(self.bookTicket.add_ticket("jean", "jean@gmail.com"), 
             [ {'email': 'manzede@gmail.com', 'fullname': 'Manzede Benard'}, 
             {"fullname": "jean", "email": "jean@gmail.com" }])
         self.assertEqual(len(self.bookTicket.tickets), 2)
 
-    #Test get ticket by email
     def test_get_ticket_by_email(self):
+        """Test get ticket by email."""
         self.assertEqual(self.bookTicket.get_ticket("manzede@gmail.com"), 
             {"fullname": "Manzede Benard", "email": "manzede@gmail.com" })
 
-    #Test get ticket by email
     def test_get_ticket_by_name(self):
+        """Test get ticket by email."""
         self.assertEqual(self.bookTicket.get_ticket_by_name("Manzede Benard"), 
             {"fullname": "Manzede Benard", "email": "manzede@gmail.com" })
 
-    #Test ticket not found
     def test_ticket_not_found(self):
+        """Test ticket not found."""
         self.assertIsNone(self.bookTicket.get_ticket("annonymous@gmail.com"))
 
-    #Test remove ticket
     def test_remove_ticket(self):
+        """Test remove ticket."""
         self.bookTicket.remove_ticket("manzede@gmail.com")
         self.assertEqual(len(self.bookTicket.tickets), 0)
 
-    #Test remove ticket not found
     def test_remove_ticket_not_found(self):
+        """Test remove ticket not found."""
         self.assertIsNone(self.bookTicket.remove_ticket("annonymous@gmail.com"))
 
-    #Empty the ticket list
     def tearDown(self):
+        """Empty the ticket list."""
         self.bookTicket.tickets = []
